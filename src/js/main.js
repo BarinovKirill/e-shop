cae9a95453f51922b25fc25304aee95507c2f62b
BarinovKirill <Barinovkirill1992@gmail.com>
2020-10-31 20:04:56 +0300

    marketing card

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,14 @@
+import './components/marketing';
+
+
 var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     // direction: 'vertical',
     loop: true,
     slidesPerView: 1,
-  
+    // autoplay: {
+    //   delay: 5000,
+    // },
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
